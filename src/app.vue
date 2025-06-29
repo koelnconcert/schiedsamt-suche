99c3db83ca5b90c0121f77bf508d40c46896a4d9
Sebastian Peters <koelnconcert@googlemail.com>
2025-06-29 08:16:31 +0200

    locale de

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,12 @@
 <template>
-  <UApp>
+  <UApp :locale="de">
     <BaseHeader/>
     <div class="">
       <NuxtPage class="m-2"/>
     </div>
   </UApp>
 </template>
+
+<script setup>
+import { de } from '@nuxt/ui/locale'
+</script>
